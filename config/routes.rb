0e808adb420b7cf63160265769537d4bd0839061
shirapi <yusei29game@gmail.com>
2018-12-13 17:16:23 +0900

    Change root Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  root 'application#hello'
-=======
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
-  root 'application#home'
->>>>>>> e793717... Add Static Pages
 end
