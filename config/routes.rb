9605562f39c7715d01dfdf4ec5ad8c5930fd68f3
shirapi <yusei29game@gmail.com>
2018-12-13 10:18:47 +0900

    Add Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   root 'application#hello'
+=======
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  root 'application#home'
+>>>>>>> e793717... Add Static Pages
 end
