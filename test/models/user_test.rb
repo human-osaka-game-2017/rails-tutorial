1411103d0320861ae5fdd1f1747913806b37eeb2
shirapi <yusei29game@gmail.com>
2019-01-17 15:08:53 +0900

    Revert "Add column password"
    This reverts commit 1c305701a0099f37b579f11f0a591277c687d03f.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,8 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com",
-                     password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "user@example.com")
   end
 
   test "should be valid" do
@@ -53,15 +52,5 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
-  end
-
-  test "password should be present (nonblank)" do
-    @user.password = @user.password_confirmation = " " * 6
-    assert_not @user.valid?
-  end
-
-  test "password should have a minimum length" do
-    @user.password = @user.password_confirmation = "a" * 5
-    assert_not @user.valid?
   end
 end
